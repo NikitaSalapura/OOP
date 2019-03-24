3a22dec654f6379e9ce86dc614e23db31ad0e03c
NikitaSalapura <salapura.n@gmail.com>
2019-03-24 15:44:22 +0300

    task1

--- task1/src/by/bntu/fitr/poisit/sleepwalkers/model/logic/MatrixWorkerFromNikita.java
+++ task1/src/by/bntu/fitr/poisit/sleepwalkers/model/logic/MatrixWorkerFromNikita.java
@@ -3,19 +3,20 @@ package by.bntu.fitr.poisit.sleepwalkers.model.logic;
 import by.bntu.fitr.poisit.sleepwalkers.model.entity.Matrix;
 
 public class MatrixWorkerFromNikita {
-    private static boolean checkRow(Matrix matrix, int i, int j, int i_offset) {
+    public static boolean checkRowForLessElements(Matrix matrix, int i, int j, int i_offset) {
         return j > 0 && matrix.getElement(i + i_offset, j - 1)
-                < matrix.getElement(i, j)
+                    < matrix.getElement(i, j)
                 || matrix.getElement(i + i_offset, j) < matrix.getElement(i, j)
                 || j < matrix.getCountColumns() - 1
-                && matrix.getElement(i + i_offset, j + 1)
-                < matrix.getElement(i, j);
+                    && matrix.getElement(i + i_offset, j + 1)
+                    < matrix.getElement(i, j);
     }
 
-    private static boolean IsLocalMin(Matrix matrix, int i, int j) {
-        return !(i > 0 && checkRow(matrix, i, j, -1)
-                || checkRow(matrix, i, j, 0)
-                || i < matrix.getCountRows() - 1 && checkRow(matrix, i, j, 1));
+    public static boolean IsLocalMin(Matrix matrix, int i, int j) {
+        return !(i > 0 && checkRowForLessElements(matrix, i, j, -1)
+                || checkRowForLessElements(matrix, i, j, 0)
+                || i < matrix.getCountRows() - 1
+                    && checkRowForLessElements(matrix, i, j, 1));
     }
 
     public static int getAmountOfLocalMins(Matrix matrix) {
--- task1/test/by/bntu/fitr/poisit/sleepwalkers/model/logic/MatrixWorkerFromNikitaTest.java
+++ task1/test/by/bntu/fitr/poisit/sleepwalkers/model/logic/MatrixWorkerFromNikitaTest.java
@@ -6,13 +6,30 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixWorkerFromNikitaTest {
+    private Matrix matrix = new Matrix(new double[][]{
+            {12.2, 23.2, 1},
+            {2, 233, 100},
+            {123, 3, -2}});
+
+    @Test
+    void testCheckRow() {
+        assertFalse(MatrixWorkerFromNikita.checkRowForLessElements
+                (matrix, 1, 0, -1));
+        assertFalse(MatrixWorkerFromNikita.checkRowForLessElements
+                (matrix, 1, 0, 0));
+        assertFalse(MatrixWorkerFromNikita.checkRowForLessElements
+                (matrix, 1, 0, 1));
+    }
+
+    @Test
+    void testIsLocalMin() {
+        assertFalse(MatrixWorkerFromNikita.IsLocalMin(matrix, 0, 0));
+        assertFalse(MatrixWorkerFromNikita.IsLocalMin(matrix, 0, 1));
+        assertTrue(MatrixWorkerFromNikita.IsLocalMin(matrix, 0, 2));
+    }
 
     @Test
     void testGetAmountOfLocalMins() {
-        Matrix matrix = new Matrix(new double[][]{
-                {12.2, 23.2, 1},
-                {2, 233, 100},
-                {123, 3, -2}});
         int expected = 3;
         int result = MatrixWorkerFromNikita.getAmountOfLocalMins(matrix);
         assertEquals(expected, result);
