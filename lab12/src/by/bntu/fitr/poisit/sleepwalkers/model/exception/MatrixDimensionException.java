f9d7496813fe94004b01bf94b19afde3eed26ee0
NikitaSalapura <salapura.n@gmail.com>
2019-04-13 15:40:21 +0300

    lab12

---
+++ lab12/src/by/bntu/fitr/poisit/sleepwalkers/model/exception/MatrixDimensionException.java
@@ -0,0 +1,12 @@
+package by.bntu.fitr.poisit.sleepwalkers.model.exception;
+
+public class MatrixDimensionException extends Exception {
+
+    public MatrixDimensionException() {
+        super();
+    }
+
+    public MatrixDimensionException(String message) {
+        super(message);
+    }
+}
---
+++ lab12/src/by/bntu/fitr/poisit/sleepwalkers/util/exception/InvalidInputException.java
@@ -0,0 +1,12 @@
+package by.bntu.fitr.poisit.sleepwalkers.util.exception;
+
+public class InvalidInputException extends Exception{
+
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
